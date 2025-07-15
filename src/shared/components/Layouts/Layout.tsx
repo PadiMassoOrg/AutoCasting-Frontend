43163cf4816e40ff53689996e52bce8a99be976b
Tomas Padilla <padillatomasagustin@gmail.com>
2025-07-15 12:39:54 +0200

    [ACFE-7] BG for Layout

--- src/shared/components/Layouts/Layout.tsx
+++ src/shared/components/Layouts/Layout.tsx
@@ -4,10 +4,8 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <>
-      <div className="h-screen w-full bg-[var(--color-primary-white)] lg:bg-[#F9FAFC]">
-        <div className="flex flex-col justify-center h-full w-[95%] max-w-[1366px] m-auto p-4">{children}</div>
-      </div>
-    </>
+    <div className="h-screen w-full bg-[var(--color-primary-white)] lg:bg-slate-50!">
+      <div className="flex flex-col justify-center h-full w-[95%] max-w-[1366px] m-auto p-4">{children}</div>
+    </div>
   );
 }
