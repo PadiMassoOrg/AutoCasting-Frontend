aaa46228c0f09ad6ca2ec62c5d58d578872e9531
Tomas Padilla <padillatomasagustin@gmail.com>
2025-08-14 00:06:05 +0200

    [ACFE-7] TEST - Deploy de VERCEL

--- src/features/profile/pages/PublicProfilePage.tsx
+++ src/features/profile/pages/PublicProfilePage.tsx
@@ -1,6 +1,5 @@
 // import { useParams } from 'react-router-dom';
 // import { usePublicProfile } from '../hooks/usePublicProfile';
-import { useTranslation } from 'react-i18next';
 import mock from '../MOCK_PROFILE.json';
 import { ViewerActions, BasicInfoSection, MediaSection } from '../components/';
 import Separator from '../../../shared/components/Separator/Separator';
@@ -9,14 +8,12 @@ import SocialMediaSection from '../components/SocialMediaSection';
 
 const PublicProfilePage = () => {
   // const { slug } = useParams<{ slug: string }>();
-  const { t } = useTranslation();
   // const { data, isLoading, error } = usePublicProfile(slug!);
-
   const error = false;
   const data = mock;
   const isLoading = false;
 
-  const { basicInfo, contact, socialMedia, media } = data;
+  const { basicInfo, socialMedia, media } = data;
 
   if (isLoading) return <p>Cargando perfil público...</p>;
   if (error || !data) return <p>Error al cargar el perfil</p>;
