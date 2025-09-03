ee42ef89aae081a0310750d8b089a6a58602d0a5
Tomas Padilla <padillatomasagustin@gmail.com>
2025-09-03 10:28:22 +0200

    [ACFE-7] Fixes for Auth Height

--- src/layouts/AuthenticationLayout.tsx
+++ src/layouts/AuthenticationLayout.tsx
@@ -4,8 +4,23 @@ type LayoutProps = {
 
 export default function AuthenticationLayout({ children }: LayoutProps) {
   return (
-    <div className="h-screen w-full bg-[var(--color-primary-white)] lg:bg-slate-50!">
-      <div className="flex flex-col justify-center h-full w-[95%] max-w-[1366px] m-auto p-4">{children}</div>
+    <div
+      className="
+        w-full min-h-dvh
+        bg-[var(--color-primary-white)] lg:bg-slate-50
+        overflow-y-auto overscroll-contain  /* permite scroll si se necesita */
+      "
+      style={{
+        paddingTop: 'env(safe-area-inset-top)',
+        paddingBottom: 'env(safe-area-inset-bottom)',
+        paddingLeft: 'env(safe-area-inset-left)',
+        paddingRight: 'env(safe-area-inset-right)',
+        scrollbarGutter: 'stable',
+      }}
+    >
+      <div className="flex min-h-dvh">
+        <div className="w-[95%] max-w-[1366px] mx-auto my-auto p-4">{children}</div>
+      </div>
     </div>
   );
 }
