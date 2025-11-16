4c7e49bab4322c4fb1ed77d7fbe960c180db0031
Tomas Padilla <padillatomasagustin@gmail.com>
2025-11-16 10:06:24 +0100

    [ACFE-93] Remove Dropdown

--- src/app/layouts/components/index.ts
+++ src/app/layouts/components/index.ts
@@ -1,7 +1,6 @@
-import AccountDropdown from './AccountDropdown';
 import AccountSideNav from './AccountSideNav';
 import Navbar from './Navbar';
 import NavbarDropdown from './NavbarDropdown';
 import Sidebar from './Sidebar';
 import UserModeSwitcher from './UserModeSwitcher';
-export { AccountDropdown, AccountSideNav, Navbar, NavbarDropdown, Sidebar, UserModeSwitcher };
+export { AccountSideNav, Navbar, NavbarDropdown, Sidebar, UserModeSwitcher };
