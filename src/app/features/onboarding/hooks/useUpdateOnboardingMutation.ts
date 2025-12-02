1f4f97ecd051955ee445e4039439cc42e9bc6b85
Tomas Padilla <padillatomasagustin@gmail.com>
2025-12-02 13:24:29 +0100

    [ACFE-93]  Adapt onboardingService and hook

--- src/app/features/onboarding/hooks/useUpdateOnboardingMutation.ts
+++ src/app/features/onboarding/hooks/useUpdateOnboardingMutation.ts
@@ -1,15 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { ME_DATA_CACHE_KEY } from '../../auth/services/authService';
+import type { MeDataResponse } from '../../auth/types/auth.types';
 import { patchUserOnboarding } from '../services/onboardingService';
 import type { UserOnboardingRequest } from '../types/onboarding.types';
 
 export const useUpdateOnboardingMutation = () => {
   const queryClient = useQueryClient();
 
-  return useMutation({
+  return useMutation<MeDataResponse, any, UserOnboardingRequest>({
     mutationFn: (payload: UserOnboardingRequest) => patchUserOnboarding(payload),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ME_DATA_CACHE_KEY });
+    onSuccess: (data) => {
+      queryClient.setQueryData(ME_DATA_CACHE_KEY, data);
     },
   });
 };
--- src/app/features/onboarding/services/onboardingService.ts
+++ src/app/features/onboarding/services/onboardingService.ts
@@ -1,8 +1,9 @@
 import api from '../../../shared/lib/axios';
 import { API_ROUTES } from '../../../shared/lib/routes';
+import type { MeDataResponse } from '../../auth/types/auth.types';
 import type { UserOnboardingRequest } from '../types/onboarding.types';
 
-export const patchUserOnboarding = async (body: UserOnboardingRequest) => {
+export const patchUserOnboarding = async (body: UserOnboardingRequest): Promise<MeDataResponse> => {
   const { data } = await api.patch(API_ROUTES.USER_ONBOARDING, body);
   return data;
 };
