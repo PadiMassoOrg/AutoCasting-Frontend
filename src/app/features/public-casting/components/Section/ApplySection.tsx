f4c0ce3c791f77a8dccd3a3d1db5dd8a66aee8d0
Tomas Padilla <padillatomasagustin@gmail.com>
2026-01-19 18:08:12 +0100

    [ACFE-104] Refactor & start MIS CASTINGS, falta: Table, Actions, Link to Details, Queries, etc...

--- src/app/features/public-casting/components/Section/ApplySection.tsx
+++ src/app/features/public-casting/components/Section/ApplySection.tsx
@@ -8,17 +8,19 @@ const ApplySection = () => {
   const isAuth = getAuthToken();
   const { mode } = useUserMode();
 
-  const showApplyButton = isAuth && mode === USER_MODE_TALENT;
+  const showApplySection = isAuth && mode === USER_MODE_TALENT;
 
-  return (
-    <article className="w-full rounded-xl border border-[var(--color-secondary-outline)] bg-white py-4 px-5 flex flex-col gap-4">
-      <h2 className="text-lg font-semibold">{t('casting-database.page.apply_title')}</h2>
-      <Separator className="opacity-20 my-2" />
-      <Button variant="primary" disabled={!showApplyButton}>
-        {t('general.apply')}
-      </Button>
-    </article>
-  );
+  if (showApplySection) {
+    return (
+      <article className="w-full rounded-xl border border-[var(--color-secondary-outline)] bg-white py-4 px-5 flex flex-col gap-4">
+        <h2 className="text-lg font-semibold">{t('casting-database.page.apply_title')}</h2>
+        <Separator className="opacity-20 my-2" />
+        <Button variant="primary" disabled={!showApplySection}>
+          {t('general.apply')}
+        </Button>
+      </article>
+    );
+  }
 };
 
 export default ApplySection;
