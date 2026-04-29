e624404c8d465ce8e31eaab57a15bf60c9a2a3fe
Tomas Padilla <padillatomasagustin@gmail.com>
2026-04-29 13:13:11 +0200

    [ACIT-39] Employer Profile settings Loading

--- src/app/features/employer/employer-profile-settings/components/Section/EmployerSettingsSecuritySection.tsx
+++ src/app/features/employer/employer-profile-settings/components/Section/EmployerSettingsSecuritySection.tsx
@@ -1,10 +1,19 @@
+import { DashboardLoadingLabel, DashboardSection } from '../../../../../layouts/components';
 import { SettingsSecuritySection } from '../../../../auth/change-password/components';
 import { useEmployerProfile } from '../../../employer-profile-edit/hooks/useEmployerProfile';
 
 const EmployerSettingsSecuritySection = () => {
-  const { data } = useEmployerProfile();
+  const { data, isLoading } = useEmployerProfile();
 
-  return <SettingsSecuritySection email={data?.email!} userAccountProvider={data?.userAccountProvider} />;
+  if (isLoading || !data) {
+    return (
+      <DashboardSection>
+        <DashboardLoadingLabel />
+      </DashboardSection>
+    );
+  }
+
+  return <SettingsSecuritySection email={data.email} userAccountProvider={data.userAccountProvider} />;
 };
 
 export default EmployerSettingsSecuritySection;
