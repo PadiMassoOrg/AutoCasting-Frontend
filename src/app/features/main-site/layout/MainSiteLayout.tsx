416c896e4616806cfd7e52f05a2ec63959deb41a
Tomas Padilla <padillatomasagustin@gmail.com>
2026-04-29 13:58:09 +0200

    [ACIT-39] PageLoading donde hagamos Fetches full content

--- src/app/features/main-site/layout/MainSiteLayout.tsx
+++ src/app/features/main-site/layout/MainSiteLayout.tsx
@@ -5,17 +5,18 @@ import PublicFooter from '../components/PublicFooter';
 
 const MainSiteLayout = () => {
   const { header } = useChromeBoxHeights();
+  const topSpacing = header + 56;
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <header data-site-header className="fixed inset-x-0 top-0 z-[80]">
         <Navbar />
       </header>
-      <main className="w-full min-h-[75vh]" style={{ paddingTop: `${header}px` }}>
+      <main className="w-full flex-1" style={{ paddingTop: `${topSpacing}px` }}>
         <Outlet />
       </main>
       <PublicFooter />
-    </>
+    </div>
   );
 };
 
