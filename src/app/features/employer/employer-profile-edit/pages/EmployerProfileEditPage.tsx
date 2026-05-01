454edcc2603798729266d7bb20713835a16eadc9
Tomas Padilla <padillatomasagustin@gmail.com>
2026-05-01 12:59:00 +0200

    [ACIT-51] Refactor de Loading para ProfileEdit

--- src/app/features/employer/employer-profile-edit/pages/EmployerProfileEditPage.tsx
+++ src/app/features/employer/employer-profile-edit/pages/EmployerProfileEditPage.tsx
@@ -1,5 +1,7 @@
+import type { DashboardShellSection } from 'autocasting-ui-library-padimasso';
 import { DashboardLoadingLabel, DashboardSection, DashboardShell } from 'autocasting-ui-library-padimasso';
 import { useTranslation } from 'react-i18next';
+import { SectionTitle } from '../../../../shared/components/Section';
 import ServerError from '../../../../shared/components/ServerError/ServerError';
 import { EmployerProfileBasicInfoEditSection } from '../components/Section';
 import { useEmployerProfile } from '../hooks/useEmployerProfile';
@@ -9,25 +11,32 @@ const EmployerProfileEditPage = () => {
   const { data, error, isLoading } = useEmployerProfile();
 
   if (error && !data) return <ServerError />;
-  if (isLoading || !data) {
-    return (
-      <DashboardShell>
-        <DashboardSection>
-          <DashboardLoadingLabel />
-        </DashboardSection>
-      </DashboardShell>
-    );
-  }
 
-  const secitons = [
+  const loadingSections: DashboardShellSection[] = [
     {
       key: 'basic',
       label: t('profile.pills.basic_info'),
-      render: () => <EmployerProfileBasicInfoEditSection data={data} />,
+      render: () => (
+        <DashboardSection>
+          <SectionTitle title={t('employer_profile.profile')} />
+          <DashboardLoadingLabel />
+        </DashboardSection>
+      ),
     },
   ];
 
-  return <DashboardShell title={t('employer_profile.profile')} sections={secitons}></DashboardShell>;
+  const sections: DashboardShellSection[] =
+    isLoading || !data
+      ? loadingSections
+      : [
+          {
+            key: 'basic',
+            label: t('profile.pills.basic_info'),
+            render: () => <EmployerProfileBasicInfoEditSection data={data} />,
+          },
+        ];
+
+  return <DashboardShell title={t('employer_profile.profile')} sections={sections}></DashboardShell>;
 };
 
 export default EmployerProfileEditPage;
